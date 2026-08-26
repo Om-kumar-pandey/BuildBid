ca4dd5fbd8232904e70bacae3b03988ab44a641b
Om-kumar-pandey <omkumarpandey4145@gmail.com>
2026-08-26 23:46:55 +0530

    Add method to find projects by customer
    Added method to fetch all projects by user from database.

--- src/main/java/com/marketplace/backend/ProjectRepository.java
+++ src/main/java/com/marketplace/backend/ProjectRepository.java
@@ -3,6 +3,12 @@ package com.marketplace.backend;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
+    
+    // इस मेथड से यूजर के सभी प्रोजेक्ट्स डेटाबेस से फेच किए जाएंगे
+    List<Project> findByCustomer(MarketplaceBackendApplication.MarketplaceUser customer);
+    
 }
